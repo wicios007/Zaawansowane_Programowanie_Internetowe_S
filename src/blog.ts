eeb9ce7d8dc18aa9b3c23b37d2d386cd078bf0e3
Dawid Zizius <livusss177@gmail.com>
2021-10-11 15:44:52 +0200

    lab1

---
+++ src/blog.ts
@@ -0,0 +1,20 @@
+class Blog {
+    private _id : number;
+    private _nazwa : string;
+    private _autor : Autor;
+    private _email : string;
+
+    constructor(imie : string, nazwisko : string){
+        this._imie = imie;
+        this._nazwisko = nazwisko;
+    }
+
+    get imie() : string { return this._imie; }
+    set imie(value:string) { this._imie = value; }
+
+    get nazwisko() : string { return this._nazwisko; }
+    set nazwisko(value:string) { this._nazwisko = value; }
+
+    get email() : string { return this._email; }
+    set email(value:string) { this._email = value; }
+}
