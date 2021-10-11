e5c2ba6b816825a0b1d89b043f9828531ad92b4f
lcz <lczech@example.com>
2021-10-11 13:58:41 +0200

    Lab01

---
+++ Lab01/src/Blog.ts
@@ -0,0 +1,20 @@
+class Blog {
+    private _id: number;
+    private _name: string;
+    private _author: Author;
+    private _article: string;
+    
+    // constructor(name: string, surname:string){
+    //     this._name = name;
+    //     this._surname = surname;
+    // }
+
+    // get name(): string { return this._name; }
+    // set name(value:string) { this._name = value; }
+    
+    // get surname(): string { return this._surname; }
+    // set surname(value:string) { this._surname = value; }
+
+    // get email(): string { return this._email; }
+    // set email(value:string) { this._email = value; }
+}
