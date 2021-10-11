5efe1c80ffb383ba265eefeea8f162f1b43fae42
bazmichael <mykhailo.bazyshyn@gmail.com>
2021-10-11 15:44:32 +0200

    Laboratorium 1

---
+++ src/models/autor.ts
@@ -0,0 +1,46 @@
+class Autor {
+    private id: number;
+    private imie: string;
+    private nazwisko: string;
+    private email: string;
+   
+
+    get Id() {
+        return this.id;
+    }
+
+    set Id(val) {
+        this.id = val;
+    }
+
+
+    get Imie() {
+        return this.imie;
+    }
+
+    set Imie(val) {
+        this.imie = val;
+    }
+    
+    get Nazwisko() {
+        return this.nazwisko;
+    }
+
+    set Nazwisko(val) {
+        this.nazwisko = val;
+    }
+
+    get Email() {
+        return this.email;
+    }
+
+    set Email(val) {
+        this.email = val;
+    }
+
+    constructor(imie: string, nazwisko: string) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+
+  }
