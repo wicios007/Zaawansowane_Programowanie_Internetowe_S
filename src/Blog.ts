4efde9fafe32c3fd7609b7858bcfc1ed1bbb82a7
wgolebiowska <wgolebiowska@poczta.pl>
2021-10-11 15:51:06 +0200

    Lab 1

---
+++ src/Blog.ts
@@ -0,0 +1,11 @@
+class Blog{
+    private _id:number;
+    private _nazwa:string;
+    private _autor: Autor;
+    private _artykul:Artykul[];
+
+constructor(nazwa:string autor:Autor){
+    this._nazwa
+
+}
+}
