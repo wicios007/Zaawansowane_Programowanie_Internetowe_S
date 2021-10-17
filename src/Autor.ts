10f60820460de26c4c98ebd325e58f62a02a8b97
Wojciech <84650124+wojciechignasiak@users.noreply.github.com>
2021-10-17 02:02:39 +0200

    Lab1 zad1. update

--- src/Autor.ts
+++ src/Autor.ts
@@ -1,6 +1,22 @@
 class Autor {
-  _id: number;
-  _tytul: string;
-  _tresc: string;
-  _dataUtworzenia: Date;
+  private _id: number;
+  private _imie: string;
+  private _nazwisko: string;
+  private _email: string;
+
+  constructor(id: number, imie: string, nazwisko: string, email: string) {
+    this._id = id;
+    this._imie = imie;
+    this._nazwisko = nazwisko;
+    this._email = email;
+  }
+
+  getEmail(): string {
+    return this._email;
+  }
+  seEmail(value: string) {
+    this._email = value;
+  }
+
+
 }
--- src/Blog.ts
+++ src/Blog.ts
@@ -1,17 +1,39 @@
 class Blog {
-  _id: number;
-  _nazwa: string;
-  _autor: AutomationRate;
-  _artykuly: Artykul[];
-
- get nazwa(value: string){
-  this._nazwa = value; 
- }
-
- get autor(value: Autor){
-   this._autor = value
- }
+  private _id: number;
+  private _nazwa: string;
+  private _autor: Autor;
+  private _artykuly: Artykul[];
 
 
+  getNazwa(): string {
+    return this._nazwa;
+  }
+
+  setNazwa(value: string) {
+    this._nazwa = value;
+  }
+
+  getAutor(): Autor {
+    return this._autor
+  }
+
+  setAutor(value: Autor) {
+    this._autor = value;
+  }
+
+  constructor(nazwa: string, autor: Autor){
+    this._nazwa = nazwa;
+    this._autor = autor;
+  }
+
+  dodajArtykul(artykul: Artykul): void {
+
+  }
+
+  pobierzTytulyArtykulow(): string[] {
+    return 
+  }
+
+  pobierzArtykul(tytul: string) {}
 
 }
