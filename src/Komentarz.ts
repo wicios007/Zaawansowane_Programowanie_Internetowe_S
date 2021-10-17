9ab0a7ef5d342a92e6cef9ac764405820d9af820
adam <adam@gmail.com>
2021-10-17 17:05:03 +0200

    lab1.1

---
+++ src/Komentarz.ts
@@ -0,0 +1,56 @@
+class Komentarz {
+    private _tresc:string;
+    private _data:Date;
+    private _odpowiedzi: Komentarz[];
+    private _nick: string;
+
+    constructor(tresc:string, nick:string)
+    {
+        this._tresc=tresc;
+        this._nick=nick;
+    }
+
+
+
+        getTresc(): string {
+        return this._tresc;
+      }
+    
+
+      setTresc(value: string) {
+        this._tresc = value;
+      }
+
+    
+      getData(): Date {
+        return this._data;
+      }
+    
+
+
+      setData(value: Date) {
+        return this._data = value;
+      }
+    
+
+
+      getNick(): string {
+        return this._nick;
+      }
+    
+
+      setNick(value: string) {
+        this._nick = value;
+      }
+    
+
+      dodajOdpowiedz(odpowiedz: Komentarz): void {
+        
+      }
+    
+      
+      pobierzOdpowiedzi(): Komentarz[] {
+        return this._odpowiedzi;
+      }
+    
+}
