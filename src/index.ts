b7687441e6512d2344b1ca46929eaab7cd3aecf2
Karol <dobrykmail@gmail.com>
2021-10-21 22:43:04 +0200

    Lab 1.7

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { ImportDataJSON } from './ImportDataJSON';
 function main(){
     const tabela = new AutorTable('id-tabeli');
     const zad4 = new GetValueZad4('num','add','sub');
-    const zad5 = new ImportDataJSON('data.json', 'tabela2');
+    const zad5 = new ImportDataJSON('data.json', 'viewOption','tableList','listNum','listDots');
 }
 
 main()
