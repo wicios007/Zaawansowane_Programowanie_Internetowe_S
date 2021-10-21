a13950ff092f797109818974104f6ff260c81abf
Karol <dobrykmail@gmail.com>
2021-10-21 22:22:56 +0200

    Lab 1.6

--- src/ImportDataJSON.ts
+++ src/ImportDataJSON.ts
@@ -1,19 +1,22 @@
 import {Autor} from './Autor';
 import { IAutor } from './IAutor';
 
+type Table = HTMLTableRowElement | null;
+
 export class ImportDataJSON {
   private _autorzy: Autor[];
 
-  constructor(url:string) {
-    this.json(url);
+  constructor(url:string, tableId:string) {
+    const table: HTMLTableRowElement | null = document.getElementById(tableId) as HTMLTableRowElement;
+    this.json(url, table);
   }
-  json(url:string) {
+  json(url:string, table:Table) {
     console.log(url);
     return fetch(url)
     .then(response => response.json())
     .then(data=>data.map(d=><IAutor>d))
     .then(Autor => Autor.forEach(p => {
-      console.log(p._imie,p._nazwisko);
+      table.innerHTML += `<tr><td>${p._imie}</td><td>${p._nazwisko}</td></tr>`;
     }));
   }
 }
