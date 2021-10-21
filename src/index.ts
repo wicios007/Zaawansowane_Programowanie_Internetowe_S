b782d9411a88f22afd413aa623d735511943e242
Karol <dobrykmail@gmail.com>
2021-10-21 20:53:38 +0200

    Lab 1.4

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { AutorTable } from "./GetData";
+import { GetValueZad4 } from './GetValueZad4';
 
 function main(){
-    const tabela = new AutorTable('id-tabeli')
+    const tabela = new AutorTable('id-tabeli');
+    const zad4 = new GetValueZad4('num','add','sub');
 }
 
 main()
