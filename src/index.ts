51122ceec72ff78bc72bd3473a7b1182ad836c4e
Eryk_Ciesielski <smth@smth.com>
2021-10-25 13:55:26 +0200

    lab3

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 function hello() {
     console.log("Witaj w swiecie Typescript");
 
+    
+
 }
 hello();
-
-
+function zad1() {
+    var tab = [1,2,3,4,5,6]
+    var sum = tab.reduce(function(a:number, b:number){return a + b})
+    console.log(sum)
+}
+zad1()
+function Parzyste(tab:number[])
+{
+    for(let i=0; i<tab.length;i++)
+    {
+        if(tab[i]%2==1)
+        {
+            tab[i]
+        }
+    }
+}
+function zad() {
+    var tab = [1,2,3,4,5,6]
+    var sum = tab.filter(function(a:number, i:number, tab:number[]){return a>7})
+    
+    console.log(sum)
+}
 
