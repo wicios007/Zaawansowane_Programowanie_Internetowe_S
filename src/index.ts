e0440fbd8ad3e831dbd7c9067c87afd1c3a3ba15
Karol <dobrykmail@gmail.com>
2021-10-25 11:13:07 +0200

    Lab02_2

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import {base} from './zad1'
+import {base1} from './zad1'
+import {base2} from './zad2'
 
 function main(){
-    const suma = base();
+    const suma = base1();
+    const suma_parz = base2();
 }
 
 main()
