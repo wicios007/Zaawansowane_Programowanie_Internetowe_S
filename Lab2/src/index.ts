49d10e75c18519f5e8c9146278509d2bd40f1548
Patryk_Adamczyk <default@mail.com>
2021-10-25 15:46:30 +0200

    Lab2

---
+++ Lab2/src/index.ts
@@ -0,0 +1,32 @@
+//zad1
+
+var tablica: number[] = [1,2,3,4,5,6,7,8,9];
+console.log(tablica)
+
+function suma(arr: number[]){
+ return arr.reduce((a, b) => a + b);
+
+}
+
+console.log(suma(tablica))
+
+
+
+//zad2
+var tablica: number[] = [1,2,3,4,5,6,7,8,9];
+console.log(tablica)
+
+function parzyste(parz: number[])
+{
+    var tab
+    //if(a%2==0){
+
+    //}
+}
+
+function sumaparzyste(arr: number[]){
+ return arr.reduce((a, b) => a + b);
+
+}
+
+console.log(suma(tablica))
