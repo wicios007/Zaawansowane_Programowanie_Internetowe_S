1230e4106fbcbc01237b57ca304082544339dd8c
Wojciech <wojciech_ignasiak@icloud.com>
2021-10-31 05:03:23 +0100

    Lab03 1-4

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,83 @@
-import { zad4 } from "./zad4";
+let tablica1 = [3,5,7,9,11,2];
 
-function hello() {
-  console.log("Witaj w świecie Typescript");
-  const body = document.body;
-  body.innerHTML = `<p>Witaj w świecie typescript</p>`
+let zadanie1: (tablica: number[]) => number;
 
-  let z4 = new zad4()
-  
+zadanie1 = function(tablica: number[]): number
+{
+  let suma: number = 0;
+  tablica.forEach(element => {
+    suma += element;
+  });
+  return suma;
 }
-hello();
+
+console.log("---------zad1-----------");
+console.log(zadanie1(tablica1));
+
+
+let zadanie2: (tablica: number[]) => number;
+
+zadanie2 = function(tablica: number[]): number
+{
+  let suma: number = 0;
+  
+  tablica.forEach(element => {
+    if (element % 2 == 0){
+    suma += element;
+    }
+  });
+  return suma
+}
+
+console.log("---------zad2-----------");
+console.log(zadanie2(tablica1));
+
+
+let zadanaLiczba = 8;
+let zadanie3: (tablica: number[], zadanaLiczba: number) => number;
+
+zadanie3 = function(tablica: number[], zadanaLiczba: number): number
+{
+  let suma: number = 0;
+  
+  tablica.forEach(element => {
+    if (element > zadanaLiczba){
+    suma += element;
+    }
+  });
+  return suma
+}
+
+console.log("---------zad3-----------");
+console.log(zadanie3(tablica1, zadanaLiczba));
+
+
+let wiekszeOd:(zadanaLiczba: number, element: number) => boolean;
+
+wiekszeOd = function(zadanaLiczba: number, element:number): boolean {
+  let wynikSprawdzenia = false;
+  if(element>zadanaLiczba){
+    wynikSprawdzenia = true
+  }
+  return true;
+}
+
+let zadanie4:(tablica: number[], zadanaLiczba: number) => number;
+
+zadanie4 = function(tablica: number[], zadanaLiczba: number): number
+{
+  let suma: number = 0;
+  
+  tablica.forEach(element => {
+    if (wiekszeOd(zadanaLiczba, element) == true){
+    suma += element;
+    }
+  });
+  return suma
+}
+
+console.log("---------zad4-----------");
+console.log(zadanie4(tablica1, zadanaLiczba));
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,36 @@
 var htmlWebpackPlugin = require("html-webpack-plugin");
 var path = require("path");
+
 module.exports = {
-  mode: "development",
-  devtool: "source-map",
-  entry: {
-    app: "./src/index.ts",
-  },
-  output: {
-    path: path.join(__dirname, "./build"),
-    filename: "bundle.js",
-  },
-  resolve: { extensions: [".js", ".ts"] },
-  devServer: {
-    static: path.join(__dirname, "./build/"),
-    port: 9000,
-  },
-  module: {
-    rules: [{ test: /\.ts$/, loader: "ts-loader" }],
-  },
-  plugins: [
-    new htmlWebpackPlugin({
-      hash: true,
-      title: "Nasza pierwsza aplikacja typescript",
-      template: "./build/index.html",
-      path: path.join(__dirname, "./build"),
-      filename: "index.html",
-    }),
-  ],
-};
+    mode: "development",
+    devtool: "source-map",
+    entry: {app:"./src/index.ts"},
+    output: {
+        path: path.join(__dirname,"./build/"),
+        filename: "bundle.js"
+    },
+    watch:true,
+    resolve:{
+        extensions: [".js",".ts"]
+    },
+    module:{
+        rules:[
+            {test:/\.ts$/, loader:"ts-loader"}
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname,'./build/'),
+        port:9000
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            hash:true,
+            title:"Nasza pierwsza aplikacja typescript",
+            myPageHeader: 'Nasza pierwsza aplikacja typescript',
+            template: './build/index.html',
+            chunks: ['vendor','shared','app'],
+            path: path.join(__dirname, "./build/"),
+            filename:'index.html' 
+        })
+    ]
+}
