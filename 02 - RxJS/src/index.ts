e57f1ecbe6c8b61899e14c7fa9526665bb1dd584
Patryk_Adamczyk <default@mail.com>
2021-11-05 11:48:52 +0100

    LAB4

--- 02 - RxJS/src/index.ts
+++ 02 - RxJS/src/index.ts
@@ -7,43 +7,49 @@ for(var i=0; i<tab.length; i++)
 }
 
 function z1(){
-    let sum = 0;
-    for (var i=0; i<tab.length; i++)
-    {
-        sum = sum + tab[i];
-    }
+    let sum = tab.reduce((a,x)=>a+x,0);
+    // for (var i=0; i<tab.length; i++)
+    // {
+    //     sum = sum + tab[i];
+    // }
     console.log("Sum of Tab Z1: "+sum);
 }
 
 function z2(){
 
-    let sum = 0;
-    for (var i=0; i<tab.length; i++)
-    {
-        if (tab[i] % 2 == 0)
-        {
-            sum = sum + tab[i];
-        }
+    let sum = tab.filter(x=>x%2 == 0).reduce((a,x)=>a+x);
+
+    // for (var i=0; i<tab.length; i++)
+    // {
+    //     if (tab[i] % 2 == 0)
+    //     {
+    //         sum = sum + tab[i];
+    //     }
         
-    }
+    // }
     console.log("Sum of Tab Z2: "+sum);
 }
 
 function z3(){
-    let sum = 0;
+    
     let greaterthan = 3;
-    for (var i=0; i<tab.length; i++)
-    {
-        if (tab[i] > greaterthan)
-        {
-            sum = sum + tab[i];
-        }
-    }
+    let sum = tab.filter(x=>x>greaterthan).reduce((a,x)=>a+x);
+    // for (var i=0; i<tab.length; i++)
+    // {
+    //     if (tab[i] > greaterthan)
+    //     {
+    //         sum = sum + tab[i];
+    //     }
+    // }
     console.log("Sum of Tab Z3: "+sum);
 }
 
 function greaterthanz4(par:number){
     let sum = 0;
+    tab.reduce(function(previousValue, currentValue, index, array) {
+        return previousValue + currentValue;
+      }, );
+
     for (var i=0; i<tab.length; i++)
     {
         if (tab[i] > par)
@@ -55,10 +61,26 @@ function greaterthanz4(par:number){
 }
 
 function z4(){
-    greaterthanz4(5);
+    greaterthanz4(5);   
+
+}
+
+function greaterthanv2(x,y){
+    return x<y;
+}
+
+function curry2<T1, T2, T3>(fn:(arg1:T1, arg2:T2)=>T3){
+    return (a1:T1)=>(a2:T2)=>fn(a1,a2);
+}
+
+function z5(){
+    var result = tab.filter(curry2(greaterthanv2)(5))
+    .reduce((a,x)=>a+x);
+    console.log("Sum of Tab Z5: "+ result)
 }
 
 z1();
 z2();
 z3();
 z4();
+z5();
