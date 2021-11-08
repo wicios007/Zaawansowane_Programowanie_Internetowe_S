9d6040055f59d04adae89c6d8bfda1b196393f81
Dawid Zizius <livusss177@gmail.com>
2021-11-08 15:47:38 +0100

    lab2(z6)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
-function hello() {
-    console.log("witaj w swiecie Typescript");
-}
+import{ Application } from "./application";
+var table : number[] = [1,2,3,4,5,6,7,8,9]
 
-hello();
+let suma = (liczby : number[]) : number => liczby.reduce((suma : number, a : number) => suma += a, 0)
+console.log("zad1 = " + suma(table));
+
+let sumapar = (liczby: number[]) : number => liczby.filter(n => n%2 == 0).reduce((suma : number, a : number) => suma += a, 0) 
+console.log("zad2 = " + sumapar(table));
+
+let sumamax = (liczby: number[],b: number) : number => liczby.filter(n => n > b).reduce((suma: number, a : number) => suma +=a, 0) 
+console.log("zad3 = " + sumamax(table,3))
+
+let filtrGreaterThen = (comp: number, numbers:number[]) : number[] => numbers.filter(n => n > comp)
+
+let filtrEven = (numbers:number[]) : number[] => numbers.filter(n => n%2 == 0)
+
+function filtrJedenParam(comp : number) : (numbers : number[]) => number[]{
+    return (numbers : number[]) : number[] => {
+        return filtrGreaterThen(comp, numbers)
+    }
+}
+let GreaterThen = filtrJedenParam(3)
+
+console.log("zad4 = " + suma(filtrEven(GreaterThen(table))))
+
+function  curry2<T1,T2,T3>(fn:(arg1:T1, arg2:T2)=>T3){
+    return(a1:T1)=>(a2:T2)=>fn(a1,a2);
+}
+let funJedenParamCurry = curry2(filtrGreaterThen)
+let filtrJedenParamCurry = funJedenParamCurry(3)
+console.log("zad5 = " +suma(filtrEven(filtrJedenParamCurry(table))))
+
+var strtable : string[] = ["Ala", "1", "Ewa", "12.4"]
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,12 @@ var path = require('path');
 module.exports = {
     mode: 'development',
     devtool: 'source-map',
-    entry: {
-        app: './src/index.ts'
-    }, 
+    entry: {app: './src/index.ts'}, 
     output:{
         path: path.join(__dirname, './build'),
         filename: 'bundle.js'
     },
+    watch:true,
     resolve: { extensions: ['.js', '.ts']},
     devServer: {
         static: path.join(__dirname, './build'),
@@ -25,6 +24,7 @@ module.exports = {
         new htmlWebpackplugin({
             hash: true,
             title: 'Nasza pierwsza aplikacja typescript',
+            myPageHeader: 'Nasza pierwsza aplikacja typescript',
             template: './build/index.html',
             path: path.join(__dirname, './build'),
             filename: 'index.html'
