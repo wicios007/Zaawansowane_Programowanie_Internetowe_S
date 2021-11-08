2c831e232eb40a5e1b0de0c8332a4b3f2ccc71dd
Kamil Brzezinski <azbx@interia.pl>
2021-11-08 13:35:36 +0100

    lab2

--- RXJS/src/index.ts
+++ RXJS/src/index.ts
@@ -2,13 +2,22 @@
 import {from,filter,map} from 'rxjs'
 
 
+let filtrWiekszeOd = (comparator:number, numbers:number[]): number[] => numbers.filter(n => n > comparator)
+function filtrJedenParameter(comparator: number):   
+    (numbers:number[]) => number[] {                
+    return (numbers:number[]):number[] => {         
+        return filtrWiekszeOd(comparator, numbers)  
+    }
+}
+let sumaLiczb = (numbers:number[]):number => numbers.reduce((sum: number, b: number) => sum += b, 0)
 
-
-    let array:Array<number>=[3,4,5,6,7,8];
-    let array2:Array<number>=[5,4,7,6,9,8];
+    let array:number[]=[3,4,5,6,7,8];
+    let array2:number[]=[5,4,7,6,9,8];
     let suma=0;
     let suma2=0;
     let suma5=0;
+    let suma6=0;
+    console.log(`filtr wieksze od : ${filtrWiekszeOd(3,array2)}`);
     array.map(e=>{
         suma+=e;
         return suma;
@@ -42,11 +51,15 @@ from(array2)
 console.log(`Suma 4 from(array) Wieksze od danej liczby(3): ${suma4}`);
 
 
-function wiekszaOd(liczba:number):(dane:Array<number>)=>number{
-    return (dane)=>{
-        return 3;
-    }
+function curry2<T1, T2, T3>(fn:(arg1:T1, arg2:T2)=>T3) {
+    return (a1:T1) => (a2:T2) => fn(a1,a2)
 }
 
+let funkcjaJedenParametrCurry = curry2(filtrWiekszeOd)
+let filtrJedenParametrCurry = funkcjaJedenParametrCurry(3)
+console.log(`funkcja curry2: ${sumaLiczb(filtrJedenParametrCurry(array2))}`)
 
+let wiekszeOd=filtrJedenParameter(3);
+
+console.log(`Funkcja jeden parametr: ${sumaLiczb(wiekszeOd(array2))}`)
 
