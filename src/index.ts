f1cc26ead17a5ddbcb0903d292e35af5b9c9b9e0
pstawiar <smth@smth.com>
2021-11-15 15:44:21 +0100

    lab5

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,51 @@
+import { interval } from "rxjs";
+import { filter, map, tap, switchMap } from "rxjs/operators";
+import { fromEvent, merge } from "rxjs";
+
 //zad1
 
-var tablica:number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for(let i = 0; i < tablica.length; i++){
-    let suma:number = 0;
-    suma = tablica[i] + suma;
-    console.log("Suma wynosi: " + suma);
-}
+const sumAll = arr => arr.reduce((a, b) => a+b);
 
+console.log(sumAll(sum));
+
+//zad 2
+
+const sumParzyste = arr => arr.filter(e =>!(e%2)).reduce((a, b) => a+b);
+
+console.log(sumParzyste(sum));
+
+//zad 3
+
+const sumWieksze = (arr, x) => arr.filter(e =>(e>x)).reduce((a, b) => a+b);
+
+console.log(sumWieksze(sum, 3));
+
+//zad 4
+const greaterThan = x => e =>(e>x)
+
+const sumWieksze2 = (arr, x) => arr.filter(greaterThan(x)).reduce((a,b)=> a+b)
+
+console.log(sumWieksze2(sum, 7));
+
+//zad 5
+
+//zad 6
+
+let string = ["Ala", "1", "Ewa", "12.4"];
+const sumString = arr => arr.filter(e => !(isNaN(e))).map(i=>Number(i)).reduce((a, b) => a+b);
+console.log(sumString(string));
+
+
+//zad 7
+
+let button = document.querySelector('.button-increase');
+let text = document.querySelector('.paragraph');
+var clicks$ = Rx.Observable
+  .fromEvent(button, 'click');
+
+clicks$.subscribe(e => {
+  console.log('increase count');
+  text.innerText = parseInt(text.innerHTML)+1;
+});
