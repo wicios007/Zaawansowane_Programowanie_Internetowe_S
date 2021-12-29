dc3b32cfd917f4141f2aab1418ddc6290b7ec93e
lcz <lczech@example.com>
2021-12-29 14:13:57 +0100

    Zad16 zrobione - działa

--- lab04/src/Zad16/Zad16.tsx
+++ lab04/src/Zad16/Zad16.tsx
@@ -17,23 +17,22 @@ export class Zad16 extends React.Component<{}, State>{
 	}
 
 	change = (e: React.ChangeEvent<HTMLInputElement>) => {
-		let newState = { ...this.state };
-		newState[e.target.name] = e.target.value;
-		this.setState(newState);
+		this.setState(state => ({ ...state, text: e.target.value, showedText: e.target.value }));
 	}
 	
 	submit = (e: React.ChangeEvent<any>) => {
 		e.preventDefault()
-		this.setState(state => ({...state, showedText: this.state.text}))
+		this.setState(state => ({...state, showedText: state.text}))
 	}
 
 	render() {
 		return(
 			<>
+			<p>{this.state.showedText}</p>
 			<form onSubmit={this.submit} id="create-form">
 				<TextField value={this.state.text} onChange={this.change} name="paragraph" label="Tekst" placeholder="Podaj tekst" fullWidth></TextField>
 			</form>
-			<Button type="submit" variant="contained" color="primary" form="create-form"></Button>
+			{/* <Button type="submit" variant="contained" color="primary" form="create-form">Zatwierdź</Button> */}
 			</>
 		)
 	}
